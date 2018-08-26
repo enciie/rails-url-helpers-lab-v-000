786880aa9b691a7c3189177bafadcda9775df77e
Nancy Jo <enciie@gmail.com>
2018-08-26 04:44:09 +0000

    automatically backed up by learn

--- db/migrate/002_add_active_column_to_students.rb
+++ db/migrate/002_add_active_column_to_students.rb
@@ -1,4 +1,4 @@
-class AddActiveColumnToStudents < ActiveReord::Migration
+class AddActiveColumnToStudents < ActiveRecord::Migration
   def change
     add_column :students, :active, :boolean, default: false
   end
